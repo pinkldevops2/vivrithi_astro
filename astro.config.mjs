2454b3b06d33d0f17aafc5e2fb49ed59454c6e4a
pinkldevops2 <devops2@pinklemonade.in>
2025-12-26 13:48:16 +0530

    Update astro.config.mjs 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
-
-// https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
+  integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  }
 });
