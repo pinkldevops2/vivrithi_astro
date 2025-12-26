53df942f1e45c0ecb4f85436b812048438938d6f
Ajith Kumar A R <>
2025-12-26 14:59:55 +0530

    Fixes3

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import nodeAdapter from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',             
-  adapter: vercel({}), 
+  adapter: nodeAdapter({ mode: 'standalone' }),
   vite: {
     plugins: [tailwindcss()]
   }
